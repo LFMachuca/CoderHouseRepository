e93773504527353ce049bbd6a21440038141b946
LFMachuca <fmigra87@gmail.com>
2024-08-29 05:38:01 -0300

    modificacion de toda la web

--- js/scripts.js
+++
@@ -1,4 +0,0 @@
-// FUNCION DEL BOTON DE LA SECCION DE PELEADORES(Muestra u oculta la lista de categorias de peso)
-function myFunction(){
-    document.getElementById("myDropdown").classList.toggle("ver");
-}
